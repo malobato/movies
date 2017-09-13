90c9a7e902e6cb88b4221fb7bb782ae161e129ee
malobato <malobato@gipym.com>
2017-09-13 14:04:24 +0200

    Stored filter to show the same results when show a movie.

--- src/main/java/com/coderszone/views/MoviesView.java
+++ src/main/java/com/coderszone/views/MoviesView.java
@@ -13,8 +13,6 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.data.sort.Sort;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.data.util.filter.SimpleStringFilter;
-import com.vaadin.data.validator.StringLengthValidator;
-import com.vaadin.event.FieldEvents.TextChangeEvent;
 import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
@@ -23,8 +21,6 @@ import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
-import com.vaadin.ui.Grid.HeaderCell;
-import com.vaadin.ui.Grid.HeaderRow;
 import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.Grid.SingleSelectionModel;
 import com.vaadin.ui.HorizontalLayout;
@@ -56,66 +52,47 @@ public class MoviesView extends UI {
 		title.setCaptionAsHtml( true );
 		title.setCaption( "<h1>" + FontAwesome.VIDEO_CAMERA.getHtml() + " Movies</h1>" );
 
+		TextField searchText = new TextField();
+
 		Grid grid = new Grid();
-		loadMovies( grid );
-		
-		// Filter row
-//		HeaderRow filterRow = grid.appendHeaderRow();
-//		
-//		HeaderCell cell = filterRow.getCell( "title" );
-//		
-//		TextField filterField = new TextField();
-//
-//		filterField.setImmediate( true );
-//		filterField.setInputPrompt( "Title" );
-//		filterField.addTextChangeListener( change -> {
-//			
-//			BeanItemContainer<Movie> container = (BeanItemContainer<Movie>) grid.getContainerDataSource(); 
-//			
-//			container.removeContainerFilters( "title" );
-//			
-//			if ( !change.getText().isEmpty() ) {
-//				
-//				container.addContainerFilter( new SimpleStringFilter( "title", change.getText(), true, false ) );
-//			}
-//		} );
-//			
-//		cell.setComponent( filterField );
-		
+		loadMovies( searchText, grid );
 		grid.setSizeFull();
 		grid.removeColumn( "description" );
 		grid.removeColumn( "poster" );
 		grid.removeColumn( "image" );
 		grid.removeColumn( "movieId" );
 		grid.setColumnOrder( "title", "year", "rating", "duration" );
+		grid.getColumn( "year" ).setWidth( 100 );
+		grid.getColumn( "rating" ).setWidth( 100 );
+		grid.getColumn( "duration" ).setWidth( 100 );
+		grid.getColumn( "directors" ).setWidth( 500 );
 		grid.setSortOrder( Sort.by( "title", SortDirection.ASCENDING ).build() );
 		grid.setSelectionMode( SelectionMode.SINGLE );
-		grid.addSelectionListener( selectionEvent -> movieClick( grid ) );
+		grid.addSelectionListener( selectionEvent -> movieClick( searchText, grid ) );
 
-		TextField searchTxt = new TextField();
-		searchTxt.setInputPrompt( "Filter movie" );
-		searchTxt.setWidth( "100%" );
-		searchTxt.focus();
-		searchTxt.addTextChangeListener( textChangeEvent -> filterMovieList( textChangeEvent, grid ) );
-		
+		searchText.setInputPrompt( "Filter movie" );
+		searchText.setWidth( "100%" );
+		searchText.focus();
+		searchText.addTextChangeListener( textChangeEvent -> filterMovieList( textChangeEvent.getText(), grid ) );
+
 		Button searchBtn = new Button( "Search" );
 		searchBtn.setWidthUndefined();
 		searchBtn.setStyleName( ValoTheme.BUTTON_PRIMARY );
 		searchBtn.setClickShortcut( KeyCode.ENTER, null );
-		searchBtn.addClickListener( clickEvent -> searchMovie( grid ) );
+		searchBtn.addClickListener( clickEvent -> searchMovie( searchText, grid ) );
 
 		HorizontalLayout toolBar = new HorizontalLayout();
 		toolBar.setWidth( "100%" );
 		toolBar.setMargin( false );
 		toolBar.setSpacing( true );
 		toolBar.addComponent( title );
-		toolBar.addComponent( searchTxt );
+		toolBar.addComponent( searchText );
 		toolBar.addComponent( searchBtn );
 		toolBar.setComponentAlignment( title, Alignment.MIDDLE_LEFT );
-		toolBar.setComponentAlignment( searchTxt,Alignment.MIDDLE_CENTER );
+		toolBar.setComponentAlignment( searchText, Alignment.MIDDLE_CENTER );
 		toolBar.setComponentAlignment( searchBtn, Alignment.MIDDLE_RIGHT );
-		
-		VerticalLayout container = new VerticalLayout();		
+
+		VerticalLayout container = new VerticalLayout();
 		container.setSizeFull();
 		container.setMargin( true );
 		container.setSpacing( true );
@@ -127,44 +104,47 @@ public class MoviesView extends UI {
 	}
 
 
-	private void filterMovieList( TextChangeEvent textChangeEvent, Grid grid ) {
+	private void filterMovieList( String text, Grid grid ) {
 
-		BeanItemContainer<Movie> container = (BeanItemContainer<Movie>) grid.getContainerDataSource(); 
-		
-		String text = textChangeEvent.getText();
-		
+		@SuppressWarnings( "unchecked" )
+		BeanItemContainer<Movie> container = (BeanItemContainer<Movie>) grid.getContainerDataSource();
+
 		container.removeContainerFilters( "title" );
-		
+
 		if ( !text.isEmpty() ) {
-			
+
 			container.addContainerFilter( new SimpleStringFilter( "title", text, true, false ) );
-		}			
+		}
 	}
-	
-	
-	private void searchMovie( Grid grid ) {
+
+
+	private void searchMovie( TextField searchText, Grid grid ) {
 
 		SearchMovieView searchMovieView = new SearchMovieView();
-		
+
 		searchMovieView.setPostersPath( postersPath );
 		searchMovieView.setMovieService( movieService );
-		searchMovieView.addCloseListener( event -> loadMovies( grid ) );
+		searchMovieView.addCloseListener( event -> loadMovies( searchText, grid ) );
 
 		UI.getCurrent().addWindow( searchMovieView );
 	}
 
 
-	private void loadMovies( Grid grid ) {
+	private void loadMovies( TextField searchText, Grid grid ) {
 
 		List<Movie> movies = movieService.findAll();
 
 		final BeanItemContainer<Movie> dataSource = new BeanItemContainer<>( Movie.class, movies );
 
 		grid.setContainerDataSource( dataSource );
+
+		if ( !searchText.getValue().isEmpty() ) {
+			filterMovieList( searchText.getValue(), grid );
+		}
 	}
 
 
-	private void movieClick( Grid grid ) {
+	private void movieClick( TextField searchText, Grid grid ) {
 
 		Object selected = ((SingleSelectionModel) grid.getSelectionModel()).getSelectedRow();
 
@@ -176,7 +156,7 @@ public class MoviesView extends UI {
 
 			Window window = new ShowMovieView( movie, movieService, postersPath );
 
-			window.addCloseListener( event -> loadMovies( grid ) );
+			window.addCloseListener( event -> loadMovies( searchText, grid ) );
 
 			UI.getCurrent().addWindow( window );
 		}
